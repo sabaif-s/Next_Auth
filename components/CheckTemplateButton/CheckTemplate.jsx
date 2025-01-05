e9c14b9b6ddc1272b6ec53e89c1e420eabf10f2d
sabaif-s <sebaifmuhammed33@gmail.com>
2025-01-06 02:30:17 +0300

    adding animation to homepage

---
+++ components/CheckTemplateButton/CheckTemplate.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
+
+const CheckTemplateButton = ({linkTemplate}) => {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: false });
+   const watchTemplateClicked=()=>{
+    window.open(linkTemplate, '_blank', 'noopener,noreferrer');
+   }
+    return (
+        <motion.div
+            ref={ref}
+            initial={{ opacity: 0, y: -100 }}
+            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+            exit={{ opacity: 0, y: 50 }}
+            transition={{ duration: 0.5 , delay:0.5 }}
+            style={{
+                position: 'absolute',
+                top: '10px',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                zIndex: 1000
+            }}
+        >
+            <button
+            onClick={watchTemplateClicked}
+            className='bg-gradient-to-r from-blue-600 via-blue-600 to-blue-400 text-white text-xl rounded-lg'
+                style={{
+                    padding: '10px 20px',
+                    fontSize: '16px',
+                    cursor: 'pointer'
+                }}
+            >
+                Watch Template
+            </button>
+        </motion.div>
+    );
+};
+
+export default CheckTemplateButton;
--- components/HomePage2/HomePage.jsx
+++ components/HomePage2/HomePage.jsx
@@ -1,23 +1,48 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import NavBarTwo from './NavBar2/NavBar2';
 import Image from 'next/image';
 import data from './Data.json';
-
-export async function generateStaticProps() {
-    return {
-        props: {
-            message: data.message,
-        },
-    };
-}
+import CheckTemplateButton from '../CheckTemplateButton/CheckTemplate';
+import { AnimatePresence, motion, useInView } from 'framer-motion';
 
 const HomePage = () => {
-    console.log("message:",data.message);
+    const [clickedDiv, setClickedDiv] = useState("left");
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: false });
+
+    useEffect(() => {
+        console.log("component mounted");
+    }, []);
+
     return (
-        <div className="w-full h-screen bg-white p-20">
-            <div className="w-full h-full bg-black flex justify-center items-center relative">
-                <div className="absolute left-0 h-full w-1/12" style={{ borderRight: "1px solid #f5f5f5" }}>
+        <motion.div
+       
+            className="w-full h-screen bg-white p-20 relative"
+            initial={{ opacity: 0 }}
+            animate={isInView ? { opacity: 1 } : { opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+        >
+            <AnimatePresence>
+                {true && (
+                    <CheckTemplateButton   key="check-template-button" linkTemplate={data.templateFirst} />
+                )}
+            </AnimatePresence>
+
+            <motion.div
+                className="w-full h-full bg-black flex justify-center items-center relative"
+                initial={{ opacity: 0, y: 50 }}
+                animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+                transition={{ duration: 1 }}
+            >
+                <motion.div
+                    className="absolute left-0 h-full w-1/12"
+                    style={{ borderRight: "1px solid #f5f5f5" }}
+                    initial={{ opacity: 0, x: -50 }}
+                    animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }}
+                    transition={{ duration: 1 }}
+                >
                     <div className='flex flex-col gap-y-6 py-4 h-4/6 relative z-50 justify-end items-center'>
                         {data.socialMedia.map((social, index) => (
                             <Image
@@ -31,11 +56,17 @@ const HomePage = () => {
                             />
                         ))}
                     </div>
-                </div>
-                <div className="w-full h-2/3 flex relative justify-center items-start">
+                </motion.div>
+                <motion.div
+                   
+                    className="w-full h-2/3 flex relative justify-center items-start"
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={isInView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0.8 }}
+                    transition={{ duration: 1 }}
+                >
                     <div className="rounded-full absolute" style={{ border: "1px solid #f5f5f5", width: "260px", height: "260px" }}>
                     </div>
-                    <div className="w-1/2 h-full flex justify-end items-center">
+                    <div className="w-1/2 h-full flex justify-end items-center ">
                         <div className="w-5/6 h-full flex justify-start items-center">
                             <div className="w-11/12 h-full flex flex-col gap-y-4 justify-center items-center relative">
                                 <div className="w-full h-1/3 flex flex-row justify-start items-center">
@@ -68,11 +99,26 @@ const HomePage = () => {
                                         <img src="/homePage2/right arrow.png" className='w-10 h-6 absolute right-2' alt="" />
                                     </div>
                                 </div>
+                                <div className="w-full cursor-pointer h-12 flex gap-x-4 justify-end items-center pr-6 absolute bottom-4 right-4">
+                                    <div
+                                        className={`w-6 h-6 rounded-full  cursor-pointer z-50 relative ${clickedDiv == "left" ? "bg-yellow-300" : "bg-white"} `}
+                                        onClick={() => setClickedDiv("left")} // Toggle dark mode
+                                    ></div>
+                                    <div
+                                        className={` ${clickedDiv == "right" ? "bg-yellow-300" : "bg-white"} w-6 h-6 rounded-full  cursor-pointer relative z-50`}
+                                        onClick={() => setClickedDiv('right')} // Example event
+                                    ></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="w-1/2 h-full pl-8 py-4 flex justify-center items-center gap-x-8">
-                        <div className='w-3/4 h-full bg-red-400 relative z-50'>
+                        <motion.div
+                            className='w-3/4 h-full bg-red-400 relative z-50'
+                            initial={{ opacity: 0, x: -50 }}
+                            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }}
+                            transition={{ duration: 1 }}
+                        >
                             <Image
                                 priority
                                 src={data.images.chair}
@@ -80,8 +126,13 @@ const HomePage = () => {
                                 className='w-full h-full'
                                 alt="Chair Home"
                             />
-                        </div>
-                        <div className='w-3/4 h-full bg-green-400 relative'>
+                        </motion.div>
+                        <motion.div
+                            className='w-3/4 h-full bg-green-400 relative'
+                            initial={{ opacity: 0, x: 50 }}
+                            animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: 50 }}
+                            transition={{ duration: 1 }}
+                        >
                             <Image
                                 priority
                                 src={data.images.plant}
@@ -89,8 +140,13 @@ const HomePage = () => {
                                 className='w-full h-full'
                                 alt="Plant Home"
                             />
-                        </div>
-                        <div className='w-1/4 h-full bg-gray-400 relative'>
+                        </motion.div>
+                        <motion.div
+                            className='w-1/4 h-full bg-gray-400 relative'
+                            initial={{ opacity: 0, y: 50 }}
+                            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+                            transition={{ duration: 1 }}
+                        >
                             <Image
                                 priority
                                 src={data.images.light}
@@ -98,21 +154,41 @@ const HomePage = () => {
                                 className='w-full h-full object-cover'
                                 alt="Light Home"
                             />
-                        </div>
+                        </motion.div>
                     </div>
-                </div>
-                <div className="absolute flex justify-end items-end top-0 left-0 h-1/6 w-full" style={{ borderBottom: "1px solid #f5f5f5" }}>
+                </motion.div>
+
+                <motion.div
+                 ref={ref}
+                    className="absolute flex justify-end items-end top-0 left-0 h-1/6 w-full"
+                    style={{ borderBottom: "1px solid #f5f5f5" }}
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -50 }}
+                    transition={{ duration: 1 }}
+                >
                     <NavBarTwo />
-                </div>
-                <div className="absolute bottom-0 left-0 flex justify-end items-center w-full h-1/6" style={{ borderTop: "1px solid #f5f5f5" }}>
+                </motion.div>
+                <motion.div
+                    className="absolute bottom-0 left-0 flex justify-end items-center w-full h-1/6"
+                    style={{ borderTop: "1px solid #f5f5f5" }}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+                    transition={{ duration: 1 }}
+                >
                     <div className='w-1/6 h-full bg-yellow-400 relative z-50'>
                         <div className="absolute z-50 inset-0 -top-[4px] -left-[4px] shadow-lg shadow-yellow-400"></div>
                     </div>
-                </div>
-                <div className="left-0 bottom-0 w-1/2 h-5/6" style={{ position: "absolute", borderRight: "1px solid #f5f5f5" }}>
-                </div>
-            </div>
-        </div>
+                </motion.div>
+                <motion.div
+                    className="left-0 bottom-0 w-1/2 h-5/6"
+                    style={{ position: "absolute", borderRight: "1px solid #f5f5f5" }}
+                    initial={{ opacity: 0, x: -50 }}
+                    animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }}
+                    transition={{ duration: 1 }}
+                >
+                </motion.div>
+            </motion.div>
+        </motion.div>
     );
 };
 
